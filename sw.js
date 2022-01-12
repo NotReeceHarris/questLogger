9c07675267a9192bdf00c7b1c4f351ba75ae7c1b
Reece Harris <30208411@blackpool.ac.uk>
2022-01-12 15:24:51 +0000

    update

--- sw.js
+++ sw.js
@@ -1,29 +1,39 @@
+const staticCacheName = 'site-static-v2';
+
 self.addEventListener('install', evt => {
     console.log('Service worker installed');
 });
 
 self.addEventListener('activate', evt => {
     console.log('service worker activated');
+
+    evt.waitUntil(
+       caches.keys().then(keys=>{
+          console.log(keys)
+          return Promise.all(keys
+            .filter(key => key !== staticCacheName)
+            .map(key => caches.delete(key)))
+       })
+    )
 });
 
 self.addEventListener("install",function(event){
     event.waitUntil(
-       caches.open("our-cache").then(function(cache){
+       caches.open(staticCacheName).then(function(cache){
           cache.addAll(["/"]);
        })
     );
  });
 
 
- self.addEventListener("fetch",function(event){
-    event.respondWith(
-        fetch(event.request).then(function(response){
-           return response;
-        }).catch(function(error){
-           //We don't really need the error parameter, but if you want to use it you know how to now.
-           return caches.match(event.request).then(function (cacheRes) {
-            return cacheRes;
-           })
-        })
-     );
- });
+self.addEventListener('fetch', evt => {
+   evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+         return cacheRes || fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+               cache.put(evt.request.url, fetchRes.clone());
+            })
+         });
+      }).catch(() => caches.match('/p/fallback.html'))
+   );
+})
