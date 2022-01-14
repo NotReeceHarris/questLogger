706253731df0418f9b0f42277e06c0c129aac58d
Reece Harris <30208411@blackpool.ac.uk>
2022-01-14 11:33:13 +0000

    firebase addition

--- src/js/db.js
+++ src/js/db.js
@@ -1,8 +1,38 @@
 db.collection('Quest-storage').onSnapshot((snapshot) => {
+    var htmlElements="";
     snapshot.docChanges().forEach(change => {
         console.log(change.doc.data(), change.doc.id)
-
-        $('#tasks').append('<div class="card-panel quest white row"> <img src="/src/img/icon128.png" alt="quest thumb"> <div class="quest-details"> <div class="quest-title">'+change.doc.data()['title']+'</div><div class="quest-tasks">'+change.doc.data()['task']+'</div></div><div class="quest-delete"> <i class="material-icons">delete_outline</i> </div>')
-
+        htmlElements += '<div class="card-panel quest white row"> <img src="/src/img/icon128.png" alt="quest thumb"> <div class="quest-details"> <div class="quest-title">'+change.doc.data()['title']+'</div><div class="quest-tasks">'+change.doc.data()['task']+'</div></div><div class="quest-delete"> <button onclick="deletetask(\''+change.doc.id+'\')" style="background-color: transparent;border: none;"><i class="material-icons">delete_outline</i></button></div></div>'
     });
+    $('#tasks').html(htmlElements)
 })
+
+function deletetask(id) {
+    db.collection("Quest-storage").doc(id).delete().then(() => {
+        console.log("Document successfully deleted!");
+
+        db.collection('Quest-storage').onSnapshot((snapshot) => {
+            var htmlElements="";
+            snapshot.docChanges().forEach(change => {
+                console.log(change.doc.data(), change.doc.id)
+                htmlElements += '<div class="card-panel quest white row"> <img src="/src/img/icon128.png" alt="quest thumb"> <div class="quest-details"> <div class="quest-title">'+change.doc.data()['title']+'</div><div class="quest-tasks">'+change.doc.data()['task']+'</div></div><div class="quest-delete"> <button onclick="deletetask(\''+change.doc.id+'\')" style="background-color: transparent;border: none;"><i class="material-icons">delete_outline</i></button></div></div>'
+            });
+            $('#tasks').html(htmlElements)
+        })
+
+
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+}
+
+$( "#AddTask" ).click(function() {
+    db.collection("Quest-storage").add({
+        title: $("#titleInput").val(),
+        task: $("#tasksInput").val()
+    })
+  });
+
+  $(["#deleteTask"]).on('click', function(){
+    console.log(this.val())
+});
